44051d8ae4eb476e3a961afebaab70fcca267581
shunsukekaneko72106 <61620829+shunsukekaneko72106@users.noreply.github.com>
2023-08-03 10:03:01 +0900

    webpackの修正とAPIからの取得

--- app/controllers/cooking_records_controller.rb
+++ app/controllers/cooking_records_controller.rb
@@ -1,4 +1,11 @@
+require 'net/http'
+require 'uri'
+
 class CookingRecordsController < ApplicationController
   def index
+    uri = URI.parse("https://cooking-records.ex.oishi-kenko.com/cooking_records")
+    response = Net::HTTP.get_response(uri)
+    @cooking_records = JSON.parse(response.body)['cooking_records']
+    puts @cooking_records
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get 'cooking_records/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'cooking_records#index'
+  get 'cooking_records', to: 'cooking_records#index'
 end
